23ec14938ef2be078f3c50a0b88fb814e2d5d0c6
Flor-Rueda <flor.rueda@gmail.com>
2023-04-17 17:48:18 -0300

    Inicio proyecto final OB

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// Imports de React
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+// Importamos las hojas de estilos
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/css/index.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+    document.getElementById('root')
+)
